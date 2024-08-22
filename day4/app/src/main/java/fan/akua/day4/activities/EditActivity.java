579be94349b98eb2cadd6318d6945b2872f0c4db
Aqua <158244791+A-kua@users.noreply.github.com>
2024-08-22 11:02:59 +0800

    [Feature] 完成随堂三

---
+++ day4/app/src/main/java/fan/akua/day4/activities/EditActivity.java
@@ -0,0 +1,46 @@
+package fan.akua.day4.activities;
+
+import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import fan.akua.day4.R;
+
+public class EditActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edittext);
+//        EditText editText = findViewById(R.id.edit);
+        //android:windowSoftInputMode="adjustResize" activity
+        // adjustPan上移
+        // adjustResize压缩
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        // 注意selfChange
+//        editText.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//
+//            }
+//        });
+
+    }
+}
