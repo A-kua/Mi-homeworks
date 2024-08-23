e2ae5e537296f29f2117816efd191505aa1cb5c0
Aqua <158244791+A-kua@users.noreply.github.com>
2024-08-23 09:43:09 +0800

    [Feature] 实现随堂3

---
+++ day5/app/src/main/java/fan/akua/day5/activities/RelativeActivity.java
@@ -0,0 +1,16 @@
+package fan.akua.day5.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import fan.akua.day5.R;
+
+public class RelativeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative);
+    }
+}
